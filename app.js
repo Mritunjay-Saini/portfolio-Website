b628f78128756582f835b7a6da41320f1d594d01
Mritunjay Saini <140881890+Mritunjay-Saini@users.noreply.github.com>
2025-06-14 07:54:59 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const personalInfo = {
         name: 'MRITUNJAY SAINI',
         title: 'Web Developer',
-        avatar: 'https://images.unsplash.com/photo-1606663889134-b1dedb5ed8b7?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        avatar: 'photo.jpg',
         email: 'sainimritunjay2003@gmail.com',
         phone: '+91 9950677356',
         birthday: '15 Dec 2003',
